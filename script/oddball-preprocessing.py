8d41796c8e64b0bace8e6a93408f5d709b6bdd64
weiyongxu <subtlelord@gmail.com>
2022-11-12 00:44:41 +0200

    rename scripts

--- script/oddball-preprocessing.py
+++ script/oddball-preprocessing.py
@@ -2,21 +2,23 @@
 """
 Created on Thu Oct 20 13:57:55 2022
 
-@author: wexu
+@author: Weiyong Xu (weiyong.w.xu@jyu.fi)
 """
 
 import mne
 from mne.preprocessing import ICA
 import numpy as np
 easycap_montage = mne.channels.make_standard_montage('easycap-M1')
-    
+   
 Ids=[9,11,12,13,14,15,26,27,28,29,30,31,32,33,34]
 
 states=['attend','ignore']
 
+data_folder='Z:/psy-interbrain/Shared/Jan/CIBA120_data/raw/'
+
 for id in Ids:
     for state in states:
-        input_fname='Z:/psy-interbrain/Shared/Jan/CIBA120_data/raw/ciba%03d_ob_%s.vhdr'%(id,state)
+        input_fname=data_folder+'ciba%03d_ob_%s.vhdr'%(id,state)
         raw=mne.io.read_raw_brainvision(vhdr_fname=input_fname,preload=True)
         raw.set_montage(easycap_montage)
         raw.plot(n_channels=32,block=True,scalings=dict(eeg=80e-6))
